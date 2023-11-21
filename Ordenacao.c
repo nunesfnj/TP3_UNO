f19163a46533458cfc13e31753adc9abadd1aadf
Haunter_kze <rafael.chang@ufv.br>
2023-11-21 20:32:41 -0300

    Update Ordenacao.c

--- Ordenacao.c
+++ Ordenacao.c
@@ -1,6 +1,31 @@
 #include "Ordenacao.h"
 
-void BubbleSort (TCarta* v, int n ){
+//definindo inteiros para contabilizar quantidade de cartas de mesma cor
+int countVerde, countAmarelo, countVermelho, countAzul;
+
+
+
+//contabiliza a quantidade de cores atraves de um case
+void Contabilizacores(TCarta *pCarta){
+    void DefineOrdem(TCarta *pCarta);
+    for(int i=0; i<10; i++){
+        switch (pCarta[i].cor){
+        case 1:
+        countVerde += 1;
+            break;
+        case 2:
+        countAmarelo += 1;
+            break;
+        case 3:
+        countVermelho += 1;
+            break;
+        case 4:
+        countAzul += 1;
+            break;
+        }
+    }
+}
+void BubbleSortCor (TCarta* v, int n ){
     int i, j;
     TCarta aux;
     for( i = 0 ; i < n-1 ; i++ ){
@@ -12,7 +37,48 @@ void BubbleSort (TCarta* v, int n ){
             } // if
         }
     }
+}
+void BubbleSortValor (TCarta* v, int n ){
+    int i, j;
+    TCarta aux;
+    for( i = 0 ; i < countVerde-1 ; i++ ){
+        for( j = 1 ; j < n-i ; j++ ){
+            if ( v[j].valor < v[j-1].valor){
+                aux = v[j];
+                v[j] = v[j-1];
+                v[j-1] = aux;
+            } // if
+        }
+    }
+    for( i += countVerde ; i < countVerde+countAmarelo-1 ; i++ ){
+        for( j = 1 ; j < n-i ; j++ ){
+            if ( v[j].valor < v[j-1].valor){
+                aux = v[j];
+                v[j] = v[j-1];
+                v[j-1] = aux;
+            } // if
+        }
+    }
+    for( i += countVerde+countAmarelo ; i < countVerde+countAmarelo+countVermelho-1 ; i++ ){
+        for( j = 1 ; j < n-i ; j++ ){
+            if ( v[j].valor < v[j-1].valor){
+                aux = v[j];
+                v[j] = v[j-1];
+                v[j-1] = aux;
+            } // if
+        }
+    }
+    for( i += countVerde+countAmarelo+countVermelho ; i < countVerde+countAmarelo+countVermelho+countAzul-1 ; i++ ){
+        for( j = 1 ; j < n-i ; j++ ){
+            if ( v[j].valor < v[j-1].valor){
+                aux = v[j];
+                v[j] = v[j-1];
+                v[j-1] = aux;
+            } // if
+        }
+    }
 }    
+
 
 void Selectsort (TCarta* v, int n){
     int i, j, Min;
@@ -29,3 +95,11 @@ void Selectsort (TCarta* v, int n){
         }
     }
 }
+
+void ImprimeMaoOrdenada(TCarta* pCarta){
+    void Destroca(TCarta* pCarta);
+    for (int i = 0; i < 10; i++){
+        printf("Cor carta %s: ", i+1);
+        printf("Valor carta %d: ", i+1);
+        }
+} 
