efd0a46ef6fd65285ea93357ceb44f5b1e6e3477
Haunter_kze <rafael.chang@ufv.br>
2023-11-21 20:32:24 -0300

    Update Ordenacao.h

--- Ordenacao.h
+++ Ordenacao.h
@@ -1,4 +1,7 @@
 #include "Carta.h"
 
+void ContabilizaCores(TCarta* pCarta);
 void BubbleSort(TCarta* pCarta, int n);
+void BubbleSortValor (TCarta* v, int n );
 void SelectSort(TCarta* pCarta, int n);
+void ImprimeMaoOrdenada(TCarta* pCarta);
