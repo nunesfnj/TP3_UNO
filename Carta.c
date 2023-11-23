3a6b1aefab4898e2e5e42eb51f29c36183f95bcf
nunesfnj <90891422+nunesfnj@users.noreply.github.com>
2023-11-23 19:43:07 -0300

    Update Carta.c
    alguns erros corrigidos durante o lab

--- Carta.c
+++ Carta.c
@@ -4,10 +4,18 @@
 
 
 void MaoInicial(TCarta* pCarta){
-    for (int i = 0; i < 10; i++){
-        printf("Cor carta %s: ", i+1);
+    for (int i = 0; i < 3; i++){
+        printf("Cor carta % c: ", i+1);
         scanf(" %c", & pCarta[i].cor);
         printf("Valor carta %d: ", i+1);
         scanf("%d", & pCarta[i].valor);
     }
+}
+
+void ImprimeMao(TCarta *pCarta){
+    //falta implementar imprime
+    for (int i = 0; i < 10; i++){
+        printf("\nCarta [%d]: %d", i+1, pCarta[i]); //nao sei imprimir a carta //usar typedef enum
+    } //conferir depois tamanho da mao inicial
+
 }
