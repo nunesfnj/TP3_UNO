a787422262815e07e76307a5cf229f14ce6c3205
Haunter_kze <rafael.chang@ufv.br>
2023-11-25 00:09:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include "Ordenacao.h"
 #include <stdio.h>
+#include <time.h>
 
 int main(){
 
     TCarta mao1;
     MaoInicial(&mao1);
+    clock_t inicio = clock(); //Inicia o registro de tempo
+    BubbleSort(&mao1, 10);
+    ImprimeMao(&mao1);
+    clock_t fim = clock(); //Termina o registro de tempo
+    double tempoDecorrido = (double)(fim - inicio) / CLOCKS_PER_SEC;
 
-    BubbleSort (&mao1, 10 );
-    
     return 0;
 }
