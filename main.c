139db990325df8c36e0539ad5ffd59ac28a4da3e
Haunter_kze <rafael.chang@ufv.br>
2023-11-26 20:15:04 -0300

    main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main() {
     const int numCartas = 10;
-    TCarta cartas[numCartas]; // aqui nao deveria ser ponteiro ?
+    TCarta cartas[numCartas];
     int tipo, tipoOrdenacao;
 
     printf("Selecione (1) para o modo interativo ou (2) para o modo por Arquivo:");
@@ -23,27 +23,87 @@ int main() {
     
     switch(tipoOrdenacao){
         case 1:
-            // Aplica Bubble Sort
-            BubbleSort(cartas, numCartas); //aqui nao deveria ser &cartas?
+            BubbleSort(cartas, numCartas);
             printf("\nBubbleSort:\n");
             ImprimirMao(cartas, numCartas);
             ImprimirComplexidade();
             break;
+
         case 2:
-            SelectionSort(cartas, numCartas);
+            SelectSort(cartas, numCartas);
             printf("\nSelectSort:\n");
             ImprimirMao(cartas, numCartas);
             ImprimirComplexidade();
             break;
+        
+        case 3:
+            //InsertSort aqui
+            printf("\nInsertSort:\n");
+            ImprimirMao(cartas, numCartas);
+            ImprimirComplexidade();
+            break;
+        
+        case 4:
+            ShellSort(cartas, numCartas);
+            printf("\nShellSort:\n");
+            ImprimirMao(cartas, numCartas);
+            ImprimirComplexidade();
+            break;
+
+        case 5:
+            //QuickSort aqui
+            printf("\nQuickSort:\n");
+            ImprimirMao(cartas, numCartas);
+            ImprimirComplexidade();
+            break;
+
         case 6:
             HeapSort(cartas, &numCartas);
             printf("\nHeapSort:\n");
             ImprimirMao(cartas, numCartas);
-            //falta fazer complexidade
+            ImprimirComplexidade();
+            break;
+        
+        default:
+            printf("\nEscolha Invalida\n");
             break;
     }
-
 }
+    else if(tipo ==2){
+        char nArquivo[20];
+        int N;
+        FILE *arq;
+        
+        printf("\ndigite o caminho para o arquivo a ser lido: ");
+        scanf("%s", nArquivo);
+
+        arq = fopen(nArquivo, "r");
+        if (arq == NULL){
+            printf("Falha no arquivo");
+            return 1;
+        }
+        else{
+            printf("Arquivo Encontrado\n\n");
+        }
+
+        fscanf(arq, "%d", &N);
+        for(int i=0; i < N; i++){    
+            TCarta cartas[10];
+            for(int j=0; j <10; j++){
+            fscanf(arq, "(%d %d) ", (int*)&cartas[j].cor, &cartas[j].valor);
+            }
+            printf("Conjunto %d antes da ordenacao:\n", i + 1);
+            ImprimirMao(cartas, 10); 
+            printf("\n");
+
+            BubbleSort(cartas, numCartas);
+            printf("\nBubbleSort:\n");
+            ImprimirMao(cartas, numCartas);
+            ImprimirComplexidade();
+        }
+
+        fclose(arq);
+    }
 
     return 0;
 }
