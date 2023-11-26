46a0982c6eb483a5240bf9efd31598bb5c1b0293
Haunter_kze <rafael.chang@ufv.br>
2023-11-26 00:32:06 -0300

    main.c

--- main.c
+++ main.c
@@ -5,19 +5,37 @@
 int main() {
     const int numCartas = 10;
     TCarta cartas[numCartas];
+    int tipo, tipoOrdenacao;
 
+    printf("Selecione (1) para o modo interativo ou (2) para o modo por Arquivo:");
+    scanf("%d", &tipo);
+    
+    if(tipo == 1){
     MaoInicial(cartas);
-
+    
     // Antes da ordenação
-    printf("MAO INICIAL:\nMao de cartas:\n");
+    printf("MAO INICIAL:\n");
     ImprimirMao(cartas, numCartas);
+    
+    printf("Escolha qual o tipo de ordenacao desejada:\n");
+    printf("(1)BubbleSort - (2)SelectSort - (3)InsertSort - (4)ShellSort - (5)QuickSort - (6)HeapSort:");
+    scanf("%d",&tipoOrdenacao);
+    
+    switch(tipoOrdenacao){
+        case 1:
+            // Aplica Bubble Sort
+            BubbleSort(cartas, numCartas);
+            printf("\nBubbleSort:\n");
+            ImprimirMao(cartas, numCartas);
+            ImprimirComplexidade();
+        case 2:
+            SelectSort(cartas, numCartas);
+            printf("\nSelectSort:\n");
+            ImprimirMao(cartas, numCartas);
+            ImprimirComplexidade();
+    }
 
-    // Aplica Bubble Sort
-    BubbleSort(cartas, numCartas);
-
-    // Depois da ordenação
-    printf("\nMao de cartas:\n");
-    ImprimirMao(cartas, numCartas);
+}
 
     return 0;
 }
