ae93639fc8b1731953acb6edd5bf9613fd315714
Haunter_kze <rafael.chang@ufv.br>
2023-11-26 20:15:20 -0300

    Ordenacao.h

--- Ordenacao.h
+++ Ordenacao.h
@@ -1,14 +1,8 @@
 #include "Carta.h"
 
 void BubbleSort(TCarta cartas[], int tamanho);
-
-void SelectionSort(TCarta* pCarta, int tamanho);
-
-void InsertionSort();
-
-void ShellSort();
-
-void QuickSort(); 
+void SelectSort(TCarta* pCarta, int tamanho);
+void ShellSort(TCarta* pCarta, int tamanho);
 
 void Refaz(int Esq, int Dir, TCarta* pCarta);
 void Constroi(TCarta* pCarta, const int* tamanho);
