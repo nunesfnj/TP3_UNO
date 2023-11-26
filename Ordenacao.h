9a4855cb68098ec708ddc9fd700ac229b7cd6912
nunesfnj <90891422+nunesfnj@users.noreply.github.com>
2023-11-26 20:05:49 -0300

    Update Ordenacao.h

--- Ordenacao.h
+++ Ordenacao.h
@@ -1,5 +1,17 @@
 #include "Carta.h"
 
 void BubbleSort(TCarta cartas[], int tamanho);
-void SelectSort(TCarta* pCarta, int tamanho);
+
+void SelectionSort(TCarta* pCarta, int tamanho);
+
+void InsertionSort();
+
+void ShellSort();
+
+void QuickSort(); 
+
+void Refaz(int Esq, int Dir, TCarta* pCarta);
+void Constroi(TCarta* pCarta, const int* tamanho);
+void HeapSort(TCarta* pCarta, const int* tamanho);
+
 void ImprimirComplexidade();
