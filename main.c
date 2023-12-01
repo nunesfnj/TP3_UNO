4074f0024811e8ede6e3d5b94db025137a6fb784
Haunter_kze <rafael.chang@ufv.br>
2023-11-30 22:50:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,75 +1,81 @@
 #include "Ordenacao.h"
+//#include "OrdenacaoArquivo.h"
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
 
 int main() {
-    const int numCartas = 10;
-    TCarta cartas[numCartas];
-    int tipo, tipoOrdenacao;
+    int numCartas = 10;
+    TCarta cartas[numCartas]; 
+    int tipo;
 
     printf("Selecione (1) para o modo interativo ou (2) para o modo por Arquivo:");
     scanf("%d", &tipo);
     
     if(tipo == 1){
     MaoInicial(cartas);
+
+    
+    //criando as 6 copias de vetores para serem usados nas ordenacoes
+   TCarta copia1[numCartas];
+    MaoInicialCopia(cartas, copia1);
+
+    TCarta copia2[numCartas];
+    MaoInicialCopia(cartas, copia2);
+
+    TCarta copia3[numCartas];
+    MaoInicialCopia(cartas, copia3);
+
+    TCarta copia4[numCartas];
+    MaoInicialCopia(cartas, copia4);
+
+    TCarta copia5[numCartas];
+    MaoInicialCopia(cartas, copia5);
+
+    TCarta copia6[numCartas];
+    MaoInicialCopia(cartas, copia6);
+
     
     // Antes da ordenação
     printf("MAO INICIAL:\n");
     ImprimirMao(cartas, numCartas);
     
-    printf("Escolha qual o tipo de ordenacao desejada:\n");
-    printf("(1)BubbleSort - (2)SelectSort - (3)InsertSort - (4)ShellSort - (5)QuickSort - (6)HeapSort:");
-    scanf("%d",&tipoOrdenacao);
-    
-    switch(tipoOrdenacao){
-        case 1:
-            BubbleSort(cartas, numCartas);
-            printf("\nBubbleSort:\n");
-            ImprimirMao(cartas, numCartas);
-            ImprimirComplexidade();
-            break;
 
-        case 2:
-            SelectSort(cartas, numCartas);
-            printf("\nSelectSort:\n");
-            ImprimirMao(cartas, numCartas);
-            ImprimirComplexidade();
-            break;
-        
-        case 3:
-            //InsertSort aqui
-            printf("\nInsertSort:\n");
-            ImprimirMao(cartas, numCartas);
-            ImprimirComplexidade();
-            break;
-        
-        case 4:
-            ShellSort(cartas, numCartas);
-            printf("\nShellSort:\n");
-            ImprimirMao(cartas, numCartas);
-            ImprimirComplexidade();
-            break;
+    BubbleSort(copia1, numCartas);
+    printf("\nBubbleSort:\n");
+    ImprimirMao(copia1, numCartas);
+    ImprimirComplexidade();
 
-        case 5:
-            //QuickSort aqui
-            printf("\nQuickSort:\n");
-            ImprimirMao(cartas, numCartas);
-            ImprimirComplexidade();
-            break;
 
-        case 6:
-            HeapSort(cartas, numCartas);
-            printf("\nHeapSort:\n");
-            ImprimirMao(cartas, numCartas);
-            ImprimirComplexidade();
-            break;
-        
-        default:
-            printf("\nEscolha Invalida\n");
-            break;
+    SelectionSort(copia2, numCartas);
+    printf("\nSelectionSort:\n");
+    ImprimirMao(copia2, numCartas);
+    ImprimirComplexidade();
+
+
+    InsertionSort(copia3, numCartas); //SE DER TEMPO, voltar numCartas constante
+    printf("\nInsertionSort:\n");
+    ImprimirMao(copia3, numCartas);
+    ImprimirComplexidade(); 
+
+
+    ShellSort(copia4, numCartas);
+    printf("\nShellSort:\n");
+    ImprimirMao(copia4, numCartas);
+    ImprimirComplexidade(); 
+
+
+    QuickSort(copia5, numCartas);
+    printf("\nQuickSort:\n");
+    ImprimirMao(copia5, numCartas);
+    ImprimirComplexidade(); 
+
+
+    HeapSort(copia6, numCartas);
+    printf("\nHeapSort:\n");
+    ImprimirMao(copia6, numCartas);
+    ImprimirComplexidade(); 
     }
-}
+    
     else if(tipo ==2){
         char nArquivo[20];
         int N;
@@ -89,68 +95,62 @@ int main() {
 
         fscanf(arq, "%d", &N);
 
-    TCarta vetor[N];
+    TCarta vetor[N][10];
     MaoInicialArquivo(arq, vetor, N);
+    printf("MAO INICIAL\n");
+    ImprimirCartas(vetor, N);
 
-    // Agora você tem as cartas no vetor e pode fazer o que quiser com elas
+    /*for (int i = 0; i < N; i++) {
+       TCarta copia1[N][10];
+    MaoInicialCopiaArquivo(vetor, copia1, N);  
 
-    // Exemplo de impressão das cartas
-    for (int i = 0; i < N; i++) {
-        printf("Carta %d: Cor = %d, Tipo = %d, Valor = %d\n", i + 1, vetor[i].cor, vetor[i].tipo, vetor[i].valor);
+    TCarta copia2[N][10];
+    MaoInicialCopiaArquivo(vetor, copia2, N);  
+
+    TCarta copia3[N][10];
+    MaoInicialCopiaArquivo(vetor, copia3, N);  
+
+    TCarta copia4[N][10];
+    MaoInicialCopiaArquivo(vetor, copia4, N);  
+
+    TCarta copia5[N][10];
+    MaoInicialCopiaArquivo(vetor, copia5, N); 
+
+    TCarta copia6[N][10];
+    MaoInicialCopiaArquivo(vetor, copia6, N);
+
+        BubbleSortArquivo(copia1, 10);  // Alterei para 1 porque BubbleSortArquivo já opera em 10 cartas
+        printf("\nBubbleSort:\n");
+        ImprimirCartas(copia1, 1);
+        ImprimirComplexidade();
+
+        SelectionSortArquivo(copia2, 10);
+        printf("\nSelectionSort:\n");
+        ImprimirCartas(copia2, 1);
+        ImprimirComplexidade();
+
+        InsertionSortArquivo(copia3, 10);  // Alterei para 1 por motivo semelhante ao BubbleSort
+        printf("\nInsertionSort:\n");
+        ImprimirCartas(copia3, 1);
+        ImprimirComplexidade();
+
+        ShellSortArquivo(copia4, 10);
+        printf("\nShellSort:\n");
+        ImprimirCartas(copia4, 1);
+        ImprimirComplexidade();
+
+        QuickSortArquivo(copia5, 10);
+        printf("\nQuickSort:\n");
+        ImprimirCartas(copia5, 1);
+        ImprimirComplexidade();
+
+        HeapSortArquivo(copia6, 10);
+        printf("\nHeapSort:\n");
+        ImprimirCartas(copia6, 1);
+        ImprimirComplexidade();
     }
-
-    fclose(arq);  // Não se esqueça de fechar o arquivo
-    }return 0;
-}
-            /*while(j < 6){
-            MaoInicialArquivo(arq, cartas[i]);
-            if(j == 0){
-                printf("\nBubbleSort\n");
-                BubbleSort(cartas[i], numCartas);
-                ImprimirComplexidade();
-                j++;
-                break;
-            }
-            if(j == 1){
-                printf("\nSelectSort\n");
-                SelectSort(cartas[i], numCartas);
-                ImprimirComplexidade();
-                j++;
-                break;
-            }
-            if(j == 2){
-                printf("\nInsertSort\n");
-                //InsertSort aqui
-                ImprimirComplexidade();
-                j++;
-                break;
-            }
-            if(j ==3){
-                printf("\nShellSort\n");
-                ShellSort(cartas[i], numCartas);
-                ImprimirComplexidade();
-                j++;
-                break;
-            }
-            if(j ==4){
-                printf("\nQuickSort\n");
-                //QuickSort aqui
-                ImprimirComplexidade();
-                j++;
-                break;
-            }
-            if(j == 5){
-                printf("\nHeapSort\n");
-                HeapSort(cartas[i], numCartas);
-                ImprimirComplexidade();
-                j++;
-                break;
-            }
-            }
-        }
-        fclose(arq);
-    }
-
+*/
+    fclose(arq);
+    }   
     return 0;
 }
-*/
