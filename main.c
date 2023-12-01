f381a978798688b6c75ca7700e13e69889e59e8a
Haunter_kze <rafael.chang@ufv.br>
2023-11-30 23:43:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "Ordenacao.h"
+//#include "OrdenacaoArquivo.h"
 #include <stdio.h>
 #include <time.h>
 
@@ -54,26 +55,81 @@ int main() {
     InsertionSort(copia3, numCartas); //SE DER TEMPO, voltar numCartas constante
     printf("\nInsertionSort:\n");
     ImprimirMao(copia3, numCartas);
-    ImprimirComplexidade(); //falta fazer
+    ImprimirComplexidade(); 
 
 
     ShellSort(copia4, numCartas);
     printf("\nShellSort:\n");
     ImprimirMao(copia4, numCartas);
-    ImprimirComplexidade(); //falta fazer
+    ImprimirComplexidade(); 
 
 
     QuickSort(copia5, numCartas);
     printf("\nQuickSort:\n");
     ImprimirMao(copia5, numCartas);
-    ImprimirComplexidade(); //falta fazer
+    ImprimirComplexidade(); 
 
 
     HeapSort(copia6, numCartas);
     printf("\nHeapSort:\n");
     ImprimirMao(copia6, numCartas);
-    ImprimirComplexidade();//falta fazer complexidade
+    ImprimirComplexidade(); 
     }
+    
+    else if(tipo ==2){
+        char nArquivo[20];
+        int N;
+        FILE *arq;
+        
+        printf("\ndigite o caminho para o arquivo a ser lido: ");
+        scanf("%s", nArquivo);
 
+        arq = fopen(nArquivo, "r");
+        if (arq == NULL){
+            printf("Falha no arquivo");
+            return 1;
+        }
+        else{
+            printf("Arquivo Encontrado\n\n");
+        }
+
+        fscanf(arq, "%d", &N);
+
+    TCarta vetor[N][10];
+    MaoInicialArquivo(arq, vetor, N);
+    printf("MAO INICIAL\n");
+    ImprimirCartas(vetor, N);
+
+    for (int i = 0; i < N; i++) {
+    BubbleSortArquivo(&vetor[i], 1);  
+}
+    printf("\nBubbleSort:\n");
+    ImprimirCartas(vetor, N);  
+    ImprimirComplexidade(); //note que as complexidades estao concatenandos, um erro que nao conseguimos corrigir
+
+    for (int i = 0; i < N; i++) {
+    SelectionSortArquivo(&vetor[i], 1);  
+}
+    printf("\nSelectSort:\n");
+    ImprimirCartas(vetor, N);  
+    ImprimirComplexidade();
+
+    for (int i = 0; i < N; i++) {
+    InsertionSortArquivo(&vetor[i], 1);  
+}
+    printf("\nInsertionSort:\n");
+    ImprimirCartas(vetor, N);  
+    ImprimirComplexidade();
+
+    for (int i = 0; i < N; i++) {
+    ShellSortArquivo(&vetor[i], 1);  
+}
+    printf("\nShellSort:\n");
+    ImprimirCartas(vetor, N);  
+    ImprimirComplexidade();
+
+
+    fclose(arq);
+    }   
     return 0;
 }
